e798e5804163cd751f719b8b7cab49ccaa1dd052
henk van den berg <ecco@Hor-Vastus.local>
2016-01-06 21:17:36 +0100

    puntenwolk

--- validate.R
+++ validate.R
@@ -1,4 +1,5 @@
 require(data.table)
+require(ggplot2)
 source("rdb/mysql.R")
 
 # What can we say about the validity of found coordinates?
@@ -39,5 +40,88 @@ compute.mean.emd <- function() {
         dtm
 }
 
+read.mean.emd <- function() {
+        if (!file.exists("data/mean_emd.csv")) {
+                compute.mean.emd()
+        }
+        dtm <- data.table(read.csv(file="data/mean_emd.csv", stringsAsFactors=FALSE))
+        dtm
+}
 
+# Step 2
+# get the found spatial data and save it
+# and...
+get.spatials <- function() {
+        query <- paste("SELECT parent_datasetId as datasetId, source, coor_x, coor_y,",
+                       "method, xy_exchanged",
+                       "FROM coar_n.tspatials",
+                       "WHERE method > 0;",
+                       sep="\n")
+        dt <- as.data.table(execute.select(query))
+        message("got ", nrow(dt), " rows for spatials.")
+        write.csv(dt, file="data/spatials.csv", row.names=FALSE)
+        dt
+}
+
+read.spatials <- function() {
+        if (!file.exists("data/spatials.csv")) {
+                get.spatials()
+        }
+        dts <- data.table(read.csv(file="data/spatials.csv", stringsAsFactors=FALSE))
+        dts
+}
+
+# Step 2 (continued)
+# and...
+# compute the distance of x and y from found points to x and y of known central point from metadata
+# also compute the real distance between these points
+compute.distance <- function() {
+        dtm <- read.mean.emd()
+        setkey(dtm, datasetId)
+        dts <- read.spatials()
+        # merge tabellen
+        dt <- merge(dtm, dts)
+        # bereken de afstand van x en y met de mean x en y, houd rekening met sd
+        # formule:      dx =  abs(x - mx) - sdx
+        #               als dx < 0, 0, anders dx
+        dt[, d_x:=ifelse(abs(coor_x - mean_x) - sd_x < 0, 0, abs(coor_x - mean_x) - sd_x)]
+        dt[, d_y:=ifelse(abs(coor_y - mean_y) - sd_y < 0, 0, abs(coor_y - mean_y) - sd_y)]
+        # breng de twee afstanden terug tot een afstand tot het mean punt
+        # de afstand van twee punten in een x/y coordinatenstelsel is gelijk aan
+        # de vierkantswortel van de som van het kwadraat van het verschil tussen de twee x/y dinges
+        dt[, distance:=as.integer(sqrt((d_x)^2 + (d_y)^2))]
+        
+        dt
+}
+
+puntenwolk <- function() {
+        # 
+        dt <- compute.distance()
+        rows1 <- nrow(dt)
+        # beperk de maximale afstand
+        afs <- 10000
+        dt <- dt[d_x <= afs, ]
+        dt <- dt[d_y <= afs, ]
+        rows2 <- nrow(dt)
+        perc <- as.integer((rows2/rows1) * 100)
+        
+        p <- ggplot(dt, aes(d_x, d_y))
+        p + geom_point(aes(colour = factor(method))) +
+                ggtitle(
+                        paste("afstand tussen gevonden punt en centraal punt uit metadata voor x en y",
+                              paste("voor", rows2, "van de", rows1, "gevonden punten (", perc, "%)"),
+                              sep="\n")) +
+                scale_x_continuous("afstand x in meters") +
+                scale_y_continuous("afstand y in meters") +
+                theme_bw(base_size = 20)
+        
+}
+
+plok.puntenwolk <- function() {
+        cairo_pdf("images/puntenwolk.pdf", bg="transparent", width = 17, height = 14)
+        op <- par(bg="transparent")
+        puntenwolk()
+        dev.off()
+        par(op)
+}
 
